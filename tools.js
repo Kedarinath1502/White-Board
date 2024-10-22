a712caf9bdbf741f8984b36acd03db5672b3f258
Spartan <venkatsaikedarinath@gmail.com>
2024-10-21 23:32:20 -0700

    someprogress

---
+++ tools.js
@@ -0,0 +1,43 @@
+let toolsCont = document.querySelector(".tool-cont")
+let pencilToolCont = document.querySelector(".pencil-tool-cont")
+let eraserToolCont = document.querySelector(".eraser-cont")
+let optionsCont = document.querySelector(".opt-cont")
+let pencil = document.querySelector(".pencil")
+let eraser = document.querySelector(".eraser")
+let pencilFlag = false
+let eraserFlag = false
+let optionsFlag = true;
+
+optionsCont.addEventListener("click", (e) => {
+    optionsFlag = !optionsFlag;
+    if(optionsFlag) openTools();
+    else closeTools();
+})
+
+function openTools() {
+    let iconElem = optionsCont.children[0];
+    iconElem.classList.remove("fa-bars")
+    iconElem.classList.add("fa-times")
+    toolsCont.style.display = "flex"
+}
+
+function closeTools(){
+    let iconElem = optionsCont.children[0];
+    iconElem.classList.remove("fa-times")
+    iconElem.classList.add("fa-bars")
+    toolsCont.style.display = "none"
+    pencilToolCont.style.display = "none"
+    eraserToolCont.style.display = "none"
+}
+
+pencil.addEventListener("click", (e) => {
+    pencilFlag = !pencilFlag
+    if(pencilFlag) pencilToolCont.style.display = "block"
+    else pencilToolCont.style.display = "none"
+})
+
+eraser.addEventListener("click", (e) => {
+    eraserFlag = !eraserFlag
+    if(eraserFlag) eraserToolCont.style.display = "flex"
+    else eraserToolCont.style.display = "none"
+})
